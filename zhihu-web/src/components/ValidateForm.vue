a83f1984215782d65eb7c9b8057760737b48c72e
gejiayang <67318944+gejiayang@users.noreply.github.com>
2022-12-21 11:34:22 +0800

    feat: form组件抽离

---
+++ zhihu-web/src/components/ValidateForm.vue
@@ -0,0 +1,10 @@
+<template>
+  <form class="validate-form-container">
+    <slot name="default"></slot>
+    <div class="submit-area">
+      <slot name="submit">
+        <button type="submit" class="btn btn-primary">提交</button>
+      </slot>
+    </div>
+  </form>
+</template>
